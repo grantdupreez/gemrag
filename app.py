14a39aab9ae3473c83b51619520924d6faa6f75f
grantdupreez <31598107+grantdupreez@users.noreply.github.com>
2025-03-22 22:14:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ from streamlit_mic_recorder import speech_to_text
 import base64
 import os
 import hmac
+from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text
+from audio_recorder_streamlit import audio_recorder
+from streamlit_float import *
 
 
 def check_password():
